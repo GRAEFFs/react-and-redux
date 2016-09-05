0cf1545c5464d4b7eecdb4f5ff833275153731c1
Gabriel Graeff <gabriel_graeff@hotmail.com>
2016-09-05 10:42:27 -0300

    Create Styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 /**
  * Created by graeff on 31/08/16.
  */
+import 'babel-polyfill';
+import React from  'react';
+import { render } from 'react-dom';
+import { Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/style.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history={browserHistory} router={routes} />,
+    document.getElementById('app')
+);
